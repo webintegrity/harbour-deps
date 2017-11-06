c69d1aa8c0c74dc1a0a851dc62414d3b3c48ac92
Viktor Szakats <vszakats@users.noreply.github.com>
2017-11-06 02:23:16 +0000

    tweak

--- brotli.sh
+++ brotli.sh
@@ -49,9 +49,9 @@ _cpu="$2"
   # shellcheck disable=SC2086
   cmake . ${options} \
     '-DCMAKE_SHARED_LIBRARY_PREFIX_C=' \
-    '-BROTLIDEC_VERSION=' \
-    '-brotlienc_VERSION=' \
-    '-brotlicommon_VERSION=' \
+    '-DBROTLIDEC_VERSION=' \
+    '-Dbrotlienc_VERSION=' \
+    '-Dbrotlicommon_VERSION=' \
     "-DCMAKE_C_COMPILER=${_CCPREFIX}gcc" \
     "-DCMAKE_CXX_COMPILER=${_CCPREFIX}g++" \
     "-DCMAKE_C_FLAGS=${_CFLAGS}" \
